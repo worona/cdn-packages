4860a21d1ca9c20d0dd2d41c6634e3e1ebb497de
Luis Herranz <luisherranz@gmail.com>
2016-10-25 19:36:08 +0200

    Add site endpoint and start using async/await

--- api/src/v1/settings/core.js
+++ api/src/v1/settings/core.js
@@ -1,28 +1,24 @@
 /* eslint-disable no-unused-vars */
-export default (req, res) => {
+export default async (req, res) => {
   const service = req.params.service;
   const packages = req.db.collection('packages');
-  packages.find({
-    service,
-    core: true,
-    type: { $in: ['extension', 'theme'] },
-  }, {
-    fields: {
+  const docs = await packages.find(
+    { service, core: true, type: { $in: ['extension', 'theme'] } },
+    { fields: {
       _id: 0,
       name: 1,
       namespace: 1,
       [`cdn.${service}.prod.main`]: 1,
       [`cdn.${service}.prod.assets`]: 1,
     },
-  }).toArray((error, docs) => {
-    if (error) throw new Error('Error retrieving docs from collection packages');
-    res.json(docs.map((doc) => {
-      const { cdn, ...rest } = doc;
-      return {
-        ...rest,
-        main: doc.cdn[service].prod.main.file,
-        assets: doc.cdn[service].prod.assets,
-      };
-    }));
-  });
+  }).toArray();
+
+  res.json(docs.map((doc) => {
+    const { cdn, ...rest } = doc;
+    return {
+      ...rest,
+      main: doc.cdn[service].prod.main.file,
+      assets: doc.cdn[service].prod.assets,
+    };
+  }));
 };
--- api/src/v1/settings/index.js
+++ api/src/v1/settings/index.js
@@ -1,10 +1,14 @@
 /* eslint-disable new-cap */
 import express from 'express';
 import core from './core';
+import site from './site';
 import development from './development';
+
+const wrap = fn => (...args) => fn(...args).catch(args[2]);
 
 export default express.Router()
   .get('/', (req, res) => res.send('Worona CDN working.'))
-  .get('/core/:service', core)
+  .get('/core/:service', wrap(core))
+  .get('/site/:siteId', wrap(site))
   .get('/package-development/:service', development)
 ;
