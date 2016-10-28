3f969d0ace121a55d509a3d32390b7b17af8a55c
Luis Herranz <luisherranz@gmail.com>
2016-10-28 11:30:35 +0200

    Update package-development endpoint.

--- api/src/v1/settings/index.js
+++ api/src/v1/settings/index.js
@@ -4,11 +4,11 @@ import core from './core';
 import site from './site';
 import development from './development';
 
-const wrap = fn => (...args) => fn(...args).catch(args[2]);
+const async = fn => (...args) => fn(...args).catch(args[2]);
 
 export default express.Router()
   .get('/', (req, res) => res.send('Worona CDN working.'))
-  .get('/core/:service', wrap(core))
-  .get('/site/:siteId', wrap(site))
-  .get('/package-development/:service', development)
+  .get('/core/:service', async(core))
+  .get('/site/:siteId', async(site))
+  .get('/package-development/:service', async(development))
 ;
