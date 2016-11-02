367428212904d0c73e77f2f99c4e9cd26926debd
Luis Herranz <luisherranz@gmail.com>
2016-11-02 12:18:35 +0100

    s3-uploader /s3/sign url added

--- api/src/app.js
+++ api/src/app.js
@@ -3,10 +3,12 @@ import express from 'express';
 import mongodb from 'express-mongo-db';
 import config from './config.json';
 import settings from './v1/settings';
+import s3 from './v1/s3';
 
 const app = express();
 
 app.use(mongodb(config.mongoUrl));
 app.use('/api/v1/settings', settings);
+app.use('/api/v1/s3', s3);
 
 export default app;
---
+++ api/src/v1/s3/sign.js
@@ -0,0 +1,13 @@
+import config from '../../config.json';
+
+process.env.AWS_ACCESS_KEY_ID = config.AWS_ACCESS_KEY_ID;
+process.env.AWS_SECRET_ACCESS_KEY = config.AWS_SECRET_ACCESS_KEY;
+
+export default require('react-s3-uploader/s3router')({
+  bucket: 'worona',
+  region: 'eu-west-1', // optional
+  // signatureVersion: 'v4', // optional (use for some amazon regions: frankfurt and others)
+  headers: { 'Access-Control-Allow-Origin': '*' }, // optional
+  ACL: 'private', // this is default,
+  // getFileKeyDir: () => 'some_folder/',
+});
