15fca08073fa8c620f3584f3717bf7e0f1e382eb
Luis Herranz <luisherranz@gmail.com>
2016-11-09 18:15:19 +0100

    Site API integrated with packages. Introduce settings-live and settings-preview.

--- api/src/v1/settings/index.js
+++ api/src/v1/settings/index.js
@@ -9,6 +9,6 @@ const async = fn => (...args) => fn(...args).catch(args[2]);
 export default express.Router()
   .get('/', (req, res) => res.send('Worona CDN working.'))
   .get('/core/:service/:env', async(core))
-  .get('/site/:siteId', async(site))
+  .get('/site/:siteId/:service/:env/:type', async(site))
   .get('/package-development/:service', async(development))
 ;
--- api/src/v1/settings/site.js
+++ api/src/v1/settings/site.js
@@ -1,14 +1,32 @@
 /* eslint-disable no-unused-vars */
 export default async (req, res) => {
   const siteId = req.params.siteId;
-  const settings = req.db.collection('settings');
+  const service = req.params.service;
+  const env = req.params.env;
+  const type = req.params.type;
+  const settings = req.db.collection(`settings-${type}`);
   const packages = req.db.collection('packages');
-  const docs = await settings.find({ siteId }, { fields: { _id: 0, siteId: 0 } }).toArray();
+  const docs = await settings.find(
+    { 'woronaInfo.siteId': siteId, 'woronaInfo.active': true },
+    { fields: { _id: 0, 'woronaInfo.active': 0 } }
+  ).toArray();
   const response = [];
   for (let i = 0; i < docs.length; i += 1) {
     const doc = docs[i];
-    const pkg = await packages.findOne({ name: doc.package.name });
-    response.push({ ...doc, package: { ...pkg } });
+    const pkg = await packages.findOne(
+      { name: doc.woronaInfo.name },
+      { fields: {
+        _id: 0,
+        [`cdn.${service}.${env}.main.file`]: 1,
+        namespace: 1,
+        niceName: 1,
+        menu: 1,
+        type: 1,
+      } },
+    );
+    const { cdn, ...rest } = pkg;
+    const woronaInfo = { ...doc.woronaInfo, ...rest, main: pkg.cdn[service][env].main.file };
+    response.push({ ...doc, woronaInfo });
   }
   res.json(response);
 };
