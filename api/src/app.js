3df128e4d4f1eba7680b98b62c362e975d3f4ca3
Luis Herranz <luisherranz@gmail.com>
2016-11-24 12:42:25 +0100

    Add cors to API.

--- api/src/app.js
+++ api/src/app.js
@@ -1,12 +1,14 @@
 import 'babel-polyfill';
 import express from 'express';
 import mongodb from 'express-mongo-db';
+import cors from 'cors';
 import config from './config.json';
 import settings from './v1/settings';
 import s3 from './v1/s3';
 
 const app = express();
 
+app.use(cors());
 app.use(mongodb(config.mongoUrl));
 app.use('/api/v1/settings', settings);
 app.use('/api/v1/s3', s3);
