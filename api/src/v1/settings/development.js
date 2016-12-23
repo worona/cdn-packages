1258efc1e103f891d83ef80ab6ce2a2fd45f0587
Luis Herranz <luisherranz@gmail.com>
2016-12-23 14:29:25 +0100

    Fix development.

--- api/src/v1/settings/development.js
+++ api/src/v1/settings/development.js
@@ -3,11 +3,11 @@ export default async (req, res) => {
   const service = req.params.service;
   const env = req.params.env;
   const vendors = await packages.findOne(
-    { name: `vendors-${service}-worona` }, { [`cdn.${service}.${env}.main`]: 1 });
+    { name: `vendors-${service}-worona` }, { [`${service}.${env}.main`]: 1 });
   const core = await packages.findOne(
-    { name: `core-${service}-worona` }, { [`cdn.${service}.${env}.main`]: 1 });
+    { name: `core-${service}-worona` }, { [`${service}.${env}.main`]: 1 });
   res.json({
-    vendors: `https://cdn.worona.io/packages/${vendors.cdn[service][env].main.file}`,
-    core: `https://cdn.worona.io/packages/${core.cdn[service][env].main.file}`,
+    vendors: `https://cdn.worona.io/packages/${vendors[service][env].main.file}`,
+    core: `https://cdn.worona.io/packages/${core[service][env].main.file}`,
   });
 };
