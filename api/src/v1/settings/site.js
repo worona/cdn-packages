01c79a5e392ac1f602e0a5decbd636e001cac975
Luis Herranz <luisherranz@gmail.com>
2017-01-16 12:35:17 +0100

    Convert tags to 3-char hashes in site endpoint.

--- api/src/v1/settings/site.js
+++ api/src/v1/settings/site.js
@@ -1,3 +1,5 @@
+import { unique } from 'shorthash';
+
 /* eslint-disable no-unused-vars */
 export default async (req, res) => {
   const siteId = req.params.siteId;
@@ -8,42 +10,44 @@ export default async (req, res) => {
   const packages = req.db.collection('packages');
   const sites = req.db.collection('sites');
 
-  const site = await sites.findOne({ _id: siteId }, { fields: {
-    userIds: 0, createdAt: 0, modifiedAt: 0, status: 0 } });
+  const site = await sites.findOne({ _id: siteId }, {
+    fields: { userIds: 0, createdAt: 0, modifiedAt: 0, status: 0 },
+  });
 
-  if (!site) { res.status(404).json({ error: 'no site found' }); return; }
+  if (!site) {
+    res.status(404).json({ error: 'no site found' });
+    return;
+  }
 
-  const docs = await settings.find(
-    { 'woronaInfo.siteId': siteId, 'woronaInfo.active': true },
-    { fields: {
-      'woronaInfo.active': 0,
-      'woronaInfo.siteId': 0,
-      'woronaInfo.init': 0,
-      'woronaInfo.namespace': 0,
-    } },
-  ).toArray();
+  const docs = await settings
+    .find({ 'woronaInfo.siteId': siteId, 'woronaInfo.active': true }, {
+      fields: {
+        'woronaInfo.active': 0,
+        'woronaInfo.siteId': 0,
+        'woronaInfo.init': 0,
+        'woronaInfo.namespace': 0,
+      },
+    })
+    .toArray();
 
   if (type === 'preview') {
     docs.push({ woronaInfo: { name: 'preview-settings-app-extension-worona' } });
   }
 
   const response = [];
-  const cacheTags = [siteId];
+  const cacheTags = [ siteId ];
   const fields = {
     _id: 0,
     [`${service}.${env}.main.file`]: 1,
     [`${service}.namespace`]: 1,
-    ...{
-      dashboard: { [`${service}.menu`]: 1 },
-      app: {},
-    }[service],
+    ...{ dashboard: { [`${service}.menu`]: 1 }, app: {} }[service],
   };
 
   for (let i = 0; i < docs.length; i += 1) {
     const doc = docs[i];
     const pkg = await packages.findOne(
       { name: doc.woronaInfo.name, [service]: { $exists: true } },
-      { fields }
+      { fields },
     );
     if (pkg) {
       const { app, dashboard, ...rest } = pkg;
@@ -58,8 +62,11 @@ export default async (req, res) => {
     }
   }
 
-  response.push({ ...site, woronaInfo: { name: 'site-general-settings-worona', namespace: 'generalSite' } });
+  response.push({
+    ...site,
+    woronaInfo: { name: 'site-general-settings-worona', namespace: 'generalSite' },
+  });
 
-  res.setHeader('Cache-Tag', cacheTags.join(' '));
+  res.setHeader('Cache-Tag', cacheTags.map(tag => unique(tag).substring(0, 3)).join(' '));
   res.json(response);
-};
+}
