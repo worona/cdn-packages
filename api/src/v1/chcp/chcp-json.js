86893b6d3862e9597cab0923619f476706b0abb7
Luis Herranz <luisherranz@gmail.com>
2017-02-01 14:28:44 +0100

    Stringify the manifest.

--- api/src/v1/chcp/chcp-json.js
+++ api/src/v1/chcp/chcp-json.js
@@ -4,7 +4,7 @@ import { generateManifest } from './chcp-manifest';
 export default async (req, res) => {
   const siteId = req.params.siteId;
   const manifest = await generateManifest({ siteId });
-  const release = md5(manifest);
+  const release = md5(JSON.stringify(manifest));
   res.json({
     content_url: `https://cdn.worona.io/api/v1/chcp/site/${siteId}/`,
     release,
