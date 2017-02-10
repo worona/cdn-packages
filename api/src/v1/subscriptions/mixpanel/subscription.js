91dd4d7d1d9847d5a7899759a7014c1fa5e6aaee
Luis Herranz <luisherranz@gmail.com>
2017-02-10 18:13:33 +0100

    Start adding userId to subscriptions (WIP)

--- api/src/v1/subscriptions/mixpanel/subscription.js
+++ api/src/v1/subscriptions/mixpanel/subscription.js
@@ -4,13 +4,17 @@ import config from '../../../../config.json';
 export default async (req, res) => {
   const listSlug = req.body.listSlug;
   const email = req.body.email;
+  const subscriptions = req.db.collection('subscriptions');
+  const lists = req.db.collection('lists');
+  const users = req.db.collection('users');
   if (!listSlug || !email) throw Error('listSlug or email parameters missing');
   const status = req.params.status;
   if (status !== 'subscribe' && status !== 'unsubscribe')
     throw Error('Use only subscribe or unsubscribe options.');
-  const subscriptions = req.db.collection('subscriptions');
-  const lists = req.db.collection('lists');
   const listObject = await lists.findOne({ slug: listSlug });
+  const user = await users.findOne({ 'emails.0.address': email }, { fields: { _id: 1 } });
+  const userId = user ? user._id : null;
+  console.log(userId);
   const listName = listObject && listObject.name;
   if (!listName)
     throw Error(`The list with slug ${listSlug} is not in the database.`);
