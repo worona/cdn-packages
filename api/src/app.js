18f61ae1224b6c5e15abb8e1eb8e6d353270f959
Luis Herranz <luisherranz@gmail.com>
2017-02-13 16:42:24 +0100

    Remove analytics.

--- api/src/app.js
+++ api/src/app.js
@@ -6,7 +6,6 @@ import { urlencoded } from 'body-parser';
 import config from '../config.json';
 import settings from './v1/settings';
 import s3 from './v1/s3';
-import analytics from './v1/analytics';
 import chcp from './v1/chcp';
 import subscriptions from './v1/subscriptions';
 
@@ -17,7 +16,6 @@ app.use(urlencoded({ extended: true }));
 app.use(mongodb(config.mongoUrl));
 app.use('/api/v1/settings', settings);
 app.use('/api/v1/s3', s3);
-app.use('/api/v1/analytics', analytics);
 app.use('/api/v1/chcp', chcp);
 app.use('/api/v1/subscriptions', subscriptions);
 
