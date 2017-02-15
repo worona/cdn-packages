a2f4d820ca10cb9ab4da3ee281d330bce1ba1782
Luis Herranz <luisherranz@gmail.com>
2017-02-15 10:37:39 +0100

    New cors api working.

--- api/src/app.js
+++ api/src/app.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill';
 import express from 'express';
 import mongodb from 'express-mongo-db';
-import cors from 'cors';
+import expressCors from 'cors';
 import { urlencoded } from 'body-parser';
 import config from '../config.json';
 import settings from './v1/settings';
@@ -9,10 +9,12 @@ import s3 from './v1/s3';
 import analytics from './v1/analytics';
 import chcp from './v1/chcp';
 import subscriptions from './v1/subscriptions';
+import cors from './v1/cors';
+
 
 const app = express();
 
-app.use(cors());
+app.use(expressCors());
 app.use(urlencoded({ extended: true }));
 app.use(mongodb(config.mongoUrl));
 app.use('/api/v1/settings', settings);
@@ -20,5 +22,6 @@ app.use('/api/v1/s3', s3);
 app.use('/api/v1/analytics', analytics);
 app.use('/api/v1/chcp', chcp);
 app.use('/api/v1/subscriptions', subscriptions);
+app.use('/api/v1/cors', cors);
 
 export default app;
---
+++ api/src/v1/cors/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable new-cap */
+import express from 'express';
+import request from 'superagent';
+import { isEmpty } from 'lodash';
+
+const async = fn => (...args) => fn(...args).catch(args[2]);
+
+export default express
+  .Router()
+  .get(/\/(.+)/, async(async (req, res) => {
+    try {
+      const result = await request(req.url.replace(/^(\/)/, ''));
+      if (isEmpty(result.body))
+        res.end(result.text);
+      else
+        res.json(result.body);
+    } catch (error) {
+      res.json(error);
+    }
+  }))
+;
