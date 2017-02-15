8125d9668fe045505c4dffb7148fca329aa6ee56
Luis Herranz <luisherranz@gmail.com>
2017-02-15 17:07:48 +0100

    Remove headers.

--- api/src/v1/cors/index.js
+++ api/src/v1/cors/index.js
@@ -10,12 +10,10 @@ export default express.Router().get(
   async(async (req, res) => {
     try {
       const result = await request(req.url.replace(/^(\/)/, ''));
-      if (result.headers.link) res.set('link', result.headers.link);
-      if (result.headers['x-wp-total']) res.set('x-wp-total', result.headers['x-wp-total']);
-      if (result.headers['x-wp-totalpages'])
-        res.set('x-wp-totalpages', result.headers['x-wp-totalpages']);
-      if (isEmpty(result.body)) res.end(result.text);
-      else res.json(result.body);
+      if (isEmpty(result.body))
+        res.end(result.text);
+      else
+        res.json(result.body);
     } catch (error) {
       res.json({ error });
     }
