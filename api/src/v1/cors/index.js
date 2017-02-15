539c942ce47caba5755f54f7d36d067dcdffdcbc
Luis Herranz <luisherranz@gmail.com>
2017-02-15 16:52:10 +0100

    Include link and wp pagination headers

--- api/src/v1/cors/index.js
+++ api/src/v1/cors/index.js
@@ -5,17 +5,19 @@ import { isEmpty } from 'lodash';
 
 const async = fn => (...args) => fn(...args).catch(args[2]);
 
-export default express
-  .Router()
-  .get(/\/(.+)/, async(async (req, res) => {
+export default express.Router().get(
+  /\/(.+)/,
+  async(async (req, res) => {
     try {
       const result = await request(req.url.replace(/^(\/)/, ''));
-      if (isEmpty(result.body))
-        res.end(result.text);
-      else
-        res.json(result.body);
+      if (result.headers.link) res.set('link', result.headers.link);
+      if (result.headers['x-wp-total']) res.set('x-wp-total', result.headers['x-wp-total']);
+      if (result.headers['x-wp-totalpages'])
+        res.set('x-wp-totalpages', result.headers['x-wp-totalpages']);
+      if (isEmpty(result.body)) res.end(result.text);
+      else res.json(result.body);
     } catch (error) {
       res.json({ error });
     }
-  }))
-;
+  }),
+);
