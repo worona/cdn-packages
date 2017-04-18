012c004ed25e1ff904be92f64bc0bc44d251875f
Luis Herranz <luisherranz@gmail.com>
2017-04-18 17:15:42 +0200

    Add access control headers

--- api/src/v1/cors/index.js
+++ api/src/v1/cors/index.js
@@ -10,11 +10,14 @@ export default express.Router().get(
   async(async (req, res) => {
     try {
       const result = await request(req.url.replace(/^(\/)/, ''));
+      res.header('status', 200);
       if (isEmpty(result.body)) {
         res.end(result.text);
       } else {
         res.header('x-wp-total', result.headers['x-wp-total']);
         res.header('x-wp-totalpages', result.headers['x-wp-totalpages']);
+        res.header('link', result.headers.link);
+        res.header('access-control-expose-headers', 'x-wp-total, x-wp-totalpages');
         res.json(result.body);
       }
     } catch (error) {
