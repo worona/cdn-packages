e14732b6defcaf247b4c9b77173c8beba80dfd87
Luis Herranz <luisherranz@gmail.com>
2017-06-28 17:03:56 +0200

    Change to getSignedUrl from Sirv

--- api/src/v1/s3/sign.js
+++ api/src/v1/s3/sign.js
@@ -1,13 +1,49 @@
 import config from '../../../config.json';
 
-process.env.AWS_ACCESS_KEY_ID = config.AWS_ACCESS_KEY_ID;
-process.env.AWS_SECRET_ACCESS_KEY = config.AWS_SECRET_ACCESS_KEY;
+// process.env.AWS_ACCESS_KEY_ID = config.AWS_ACCESS_KEY_ID;
+// process.env.AWS_SECRET_ACCESS_KEY = config.AWS_ACCESS_KEY_ID;
+//
+// export const old = require('react-s3-uploader/s3router')({
+//   bucket: 'worona',
+//   region: 'eu-west-1', // optional
+//   // signatureVersion: 'v4', // optional (use for some amazon regions: frankfurt and others)
+//   headers: { 'Access-Control-Allow-Origin': '*' }, // optional
+//   ACL: 'private', // this is default,
+//   getFileKeyDir: req => `sites/${req.query.siteId}/${req.query.imgType}/`,
+// });
 
-export default require('react-s3-uploader/s3router')({
-  bucket: 'worona',
-  region: 'eu-west-1', // optional
-  // signatureVersion: 'v4', // optional (use for some amazon regions: frankfurt and others)
-  headers: { 'Access-Control-Allow-Origin': '*' }, // optional
-  ACL: 'private', // this is default,
-  getFileKeyDir: req => `sites/${req.query.siteId}/${req.query.imgType}/`,
-});
+const getSignedUrl = ({ s3, Key }) =>
+  new Promise((resolve, reject) =>
+    s3.getSignedUrl(
+      'putObject',
+      {
+        Bucket: 'worona',
+        Key,
+      },
+      (error, url) => {
+        if (error) reject(error);
+        else resolve(url);
+      }
+    )
+  );
+
+export default async (req, res) => {
+  const AWS = require('aws-sdk');
+
+  AWS.config.update({
+    accessKeyId: config.AWS_ACCESS_KEY_ID,
+    secretAccessKey: config.AWS_ACCESS_KEY_ID,
+  });
+
+  const s3 = new AWS.S3({
+    endpoint: new AWS.Endpoint('https://s3.sirv.com'),
+    s3ForcePathStyle: true,
+  });
+
+  try {
+    const url = await getSignedUrl({ s3, Key: `sites/${req.query.siteId}/${req.query.imgType}/` });
+    res.send(url);
+  } catch (error) {
+    res.send(error);
+  }
+};
