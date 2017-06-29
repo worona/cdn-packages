71e50c116b2abbe960550682e831bfc40bf595e0
Eduardo Campaña <asrielo@gmail.com>
2017-06-29 10:46:39 +0200

    Implemented aws-sdk for Sirv.

--- api/src/v1/s3/sign.js
+++ api/src/v1/s3/sign.js
@@ -1,20 +1,9 @@
 import config from '../../../config.json';
 
-// process.env.AWS_ACCESS_KEY_ID = config.AWS_ACCESS_KEY_ID;
-// process.env.AWS_SECRET_ACCESS_KEY = config.AWS_ACCESS_KEY_ID;
-//
-// export const old = require('react-s3-uploader/s3router')({
-//   bucket: 'worona',
-//   region: 'eu-west-1', // optional
-//   // signatureVersion: 'v4', // optional (use for some amazon regions: frankfurt and others)
-//   headers: { 'Access-Control-Allow-Origin': '*' }, // optional
-//   ACL: 'private', // this is default,
-//   getFileKeyDir: req => `sites/${req.query.siteId}/${req.query.imgType}/`,
-// });
-
 const getSignedUrl = ({ s3, Key }) =>
-  new Promise((resolve, reject) =>
-    s3.getSignedUrl(
+  new Promise((resolve, reject) => {
+    console.log('key:', Key);
+    return s3.getSignedUrl(
       'putObject',
       {
         Bucket: 'worona',
@@ -24,15 +13,15 @@ const getSignedUrl = ({ s3, Key }) =>
         if (error) reject(error);
         else resolve(url);
       }
-    )
-  );
+    );
+  });
 
 export default async (req, res) => {
   const AWS = require('aws-sdk');
 
   AWS.config.update({
     accessKeyId: config.AWS_ACCESS_KEY_ID,
-    secretAccessKey: config.AWS_ACCESS_KEY_ID,
+    secretAccessKey: config.AWS_SECRET_ACCESS_KEY,
   });
 
   const s3 = new AWS.S3({
@@ -41,9 +30,15 @@ export default async (req, res) => {
   });
 
   try {
-    const url = await getSignedUrl({ s3, Key: `sites/${req.query.siteId}/${req.query.imgType}/` });
+    console.log('query:', req.query);
+    const url = await getSignedUrl({
+      s3,
+      Key: `sites/${req.query.siteId}/${req.query.imgType}/${req.query.objectName}`,
+    });
+    console.log('succeed:', url);
     res.send(url);
   } catch (error) {
+    console.log('error:', error);
     res.send(error);
   }
 };
